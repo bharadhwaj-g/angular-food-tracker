49aa414c39cbefd1c98a9acf55ef6628cc38a5f5
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-03-25 09:19:32 -0700

    add new-food component

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -1,17 +1,19 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { FoodDisplayComponent } from './food-display.component';
+import { NewFoodComponent } from './new-food.component';
 import { Food } from './food.model';
 
 @Component({
   selector: 'food-list',
   inputs: ['foodList'],
   outputs: ['onFoodSelect'],
-  directives: [FoodDisplayComponent],
+  directives: [FoodDisplayComponent, NewFoodComponent],
   template: `
     <div *ngFor="#food of foodList">
       <h3 *ngIf="food !== selectedFood" (click)="foodClicked(food)">{{ food.name }}</h3>
       <food-display *ngIf="food === selectedFood" [food]="selectedFood"></food-display>
     </div>
+    <new-food (onSubmitNewFood)="addFood($event)"></new-food>
   `
 
 })
@@ -26,5 +28,11 @@ export class FoodListComponent {
   foodClicked(clickedFood: Food): void {
     this.selectedFood= clickedFood;
     this.onFoodSelect.emit(clickedFood);
+  }
+  addFood(newFood: string): void {
+    var foodCalories: number = parseInt(newFood[2]);
+    this.foodList.push(
+      new Food(newFood[0], newFood[1], foodCalories, this.foodList.length)
+    )
   }
 }
---
+++ app/new-food.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Food } from './food.model';
+
+@Component({
+  selector: 'new-food',
+  outputs: ['onSubmitNewFood'],
+  template: `
+  <div>
+    <h3>Add Food</h3>
+      <label>Enter Food Name:</label>
+      <input placeholder="name" #newName>
+      <label>Enter Details:</label>
+      <input placeholder="details" #newDetails>
+      <label>Enter Calories:</label>
+      <input placeholder="calories" #newCalories>
+      <button (click)="addFood(newName, newDetails, newCalories)">Add</button>
+  </div>
+  `
+})
+
+export class NewFoodComponent {
+  public onSubmitNewFood: EventEmitter<String[]>;
+  constructor() {
+    this.onSubmitNewFood = new EventEmitter();
+  }
+  addFood(name: HTMLInputElement, details: HTMLInputElement, calories: HTMLInputElement) {
+    this.onSubmitNewFood.emit([name.value, details.value, calories.value]);
+    name.value = "";
+    details.value = "";
+    calories.value = "";
+  }
+}
