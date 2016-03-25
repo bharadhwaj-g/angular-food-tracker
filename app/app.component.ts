1959810f5d3a3d6c7f98988e6f4cd74767adbd2b
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-03-25 08:57:33 -0700

    create sample array of foods

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from 'angular2/core';
+import { FoodListComponent } from './food-list.component';
+import { Food } from './food.model';
 
 @Component({
   selector: 'my-app',
+  directives: [FoodListComponent],
   template: `
     <div class="container">
       <h1>Food Tracker</h1>
+      <food-list [foodList]="foods"></food-list>
     </div>
   `
 })
 
 export class AppComponent {
-
+  public foods: Food[];
+  constructor(){
+    this.foods = [
+      new Food("Banana", "Ate it right when I woke up", 105, 0),
+      new Food("Eggs", "Two fried eggs", 180, 1),
+      new Food("Orange juice", "Medium glass of low pulp orange juice", 222, 2),
+      new Food("Dates", "Incessantly snacked on dates all afternoon", 532, 3),
+      new Food("Ice Cream", "Too much Ben & Jerry's Half Baked while watching TV", 640, 4),
+    ]
+  }
 }
