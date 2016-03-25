5286400f9e56dd93d904ffb2e97ced7a9c5e0328
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-03-25 08:01:27 -0700

    add initial food tracker files

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+      <h1>Food Tracker</h1>
+    </div>
+  `
+})
+
+export class AppComponent {
+
+}
