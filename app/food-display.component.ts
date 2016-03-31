9e44352612dd813cd4e92882c5c4b595f92a7249
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-03-31 08:38:00 -0700

    remove unused event emitter import

--- app/food-display.component.ts
+++ app/food-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter } from 'angular2/core';
+import { Component } from 'angular2/core';
 import { EditFoodComponent } from './edit-food.component';
 import { Food } from './food.model';
 
